4e82923e668b40a98905a043f10158800c8978fd
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2025-01-19 08:30:58 +0100

    whitelist new event factory

--- script/Setup.s.sol
+++ script/Setup.s.sol
@@ -23,12 +23,12 @@ contract Setup is Script {
         // SignerRegistry(signerRegistry).registerSigner(clientId, signer);
 
         // Deploy ContractRegistry
-        address lotteryFactory = 0x7Cf08228EC01191c2693B9539D02B37DACCC3f24;
-        // address eventFactory = 0x3279c8c8e109164690698183D5Ea6B45e50466Ae;
+        // address lotteryFactory = 0x7Cf08228EC01191c2693B9539D02B37DACCC3f24;
+        address eventFactory = 0xe34D26a7018382FbB8e0D3be55EB5D6073ff00D2;
         // address freeToPlayToken = 0x9Ff6a0DC28dfc56858BDC677E77858E00BDF7D44;
 
-        ContractRegistry(contractRegistry).setAllowedContract(clientId, lotteryFactory, true);
-        // ContractRegistry(contractRegistry).setAllowedContract(clientId, eventFactory, true);
+        // ContractRegistry(contractRegistry).setAllowedContract(clientId, lotteryFactory, true);
+        ContractRegistry(contractRegistry).setAllowedContract(clientId, eventFactory, true);
         // ContractRegistry(contractRegistry).setAllowedContract(clientId, freeToPlayToken, true);
     }
 }
