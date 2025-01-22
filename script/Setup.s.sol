a49bdb45585358b9e4a4cf9c2564aa652de4a611
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2025-01-22 16:09:24 +0100

    update signer and lottery factory addresses in Setup script

--- script/Setup.s.sol
+++ script/Setup.s.sol
@@ -14,21 +14,21 @@ contract Setup is Script {
     function run() external {
         vm.startBroadcast();
 
-        // address signerRegistry = 0x5a684137fddb5dFF6e2276906BDbf0510F022FBc;
+        address signerRegistry = 0x7B06b5f685AAA61528e754196d4FF2E296F88739;
         address contractRegistry = 0x75C4A34B13a891679241A34bEfA3c5a83bFE032a;
-        // address signer = 0x6F6623B00B0b2eAEFA47A4fDE06d6931F7121722;
+        address signer = 0x6F6623B00B0b2eAEFA47A4fDE06d6931F7121722;
         bytes32 clientId = 0xb33237270006a2cb6b24935fc83a916d366f4c2a5b9ea8b91ea3b191606c11cf;
 
         // Deploy SignerRegistry
-        // SignerRegistry(signerRegistry).registerSigner(clientId, signer);
+        SignerRegistry(signerRegistry).registerSigner(clientId, signer);
 
         // Deploy ContractRegistry
-        // address lotteryFactory = 0x7Cf08228EC01191c2693B9539D02B37DACCC3f24;
+        address lotteryFactory = 0x98A9D15C8D98C32a6f2E53A573d7A28071e71F32;
         address eventFactory = 0x827a01E3dEBdE2997C10d20b15a17b2615CBFFeF;
-        // address freeToPlayToken = 0x9Ff6a0DC28dfc56858BDC677E77858E00BDF7D44;
+        address freeToPlayToken = 0x9Ff6a0DC28dfc56858BDC677E77858E00BDF7D44;
 
-        // ContractRegistry(contractRegistry).setAllowedContract(clientId, lotteryFactory, true);
+        ContractRegistry(contractRegistry).setAllowedContract(clientId, lotteryFactory, true);
         ContractRegistry(contractRegistry).setAllowedContract(clientId, eventFactory, true);
-        // ContractRegistry(contractRegistry).setAllowedContract(clientId, freeToPlayToken, true);
+        ContractRegistry(contractRegistry).setAllowedContract(clientId, freeToPlayToken, true);
     }
 }
